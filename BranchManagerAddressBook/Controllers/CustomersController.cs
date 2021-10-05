9d78b12d816abac5b00c5a5a8bc66addd3aff25e
SW <Shaowei.Zhang@Outlook.com>
2021-10-05 22:51:06 +1100

    update controller to support delete

--- BranchManagerAddressBook/Controllers/CustomersController.cs
+++ BranchManagerAddressBook/Controllers/CustomersController.cs
@@ -117,5 +117,115 @@ namespace RefactorThis.Controllers
                 return Problem("Try to add Customer {customer.Name} with id = {customer.Id} but failed");
             }
         }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id, Customer customer)
+        {
+            var orig = new Customer(id)
+            {
+                Name = customer.Name,
+                PhoneNumber = customer.PhoneNumber,
+            };
+            var rowsAffected = 0;
+
+            if (!orig.IsNew)
+            {
+                try
+                {
+                    rowsAffected = orig.Save();
+                }
+                catch (Exception e)
+                {
+                    _messageController.LogError("Customers Update", e);
+                    throw;
+                }
+            }
+
+            if (rowsAffected > 0)
+            {
+                var found = _cache.TryGetValue("Customers", out Customers customers);
+                if (found)
+                {
+                    var customerUpdated = customers.Items.Find(customer => customer.Id == id);
+                    if (customerUpdated != null)
+                    {
+                        customerUpdated.Name = customer.Name;
+                        customerUpdated.PhoneNumber = customer.PhoneNumber;
+                        var removed = customers.Items.Remove(new Customer(id));
+                        if (removed)
+                        {
+                            customers.Items.Add(customerUpdated);
+                            _cache.Set("Customers", customers, new MemoryCacheEntryOptions()
+                                .SetSlidingExpiration(TimeSpan.FromMinutes(5)));
+                        }
+                    }
+                }
+
+                return Ok($"Customer {customer.Name} with id = {customer.Id} has been Updated.");
+            }
+            else
+            {
+                return Problem($"Try to update Customer {customer.Name} with id = {customer.Id}, but failed");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var customer = new Customer(id);
+            var rowsAffected = 0;
+            try
+            {
+                rowsAffected = customer.Delete();
+            }
+            catch (Exception e)
+            {
+                _messageController.LogError("Customers Delete", e);
+                throw;
+            }
+
+            var found = _cache.TryGetValue("Customers", out Customers customers);
+            if (found)
+            {
+                var customerUpdated = customers.Items.Find(customer => customer.Id == id);
+                if (customerUpdated != null)
+                {
+                    customers.Items.Remove(new Customer(id));
+                    _cache.Set("Customers", customers, new MemoryCacheEntryOptions()
+                        .SetSlidingExpiration(TimeSpan.FromMinutes(5)));
+                }
+            }
+
+            if (rowsAffected > 0)
+            {
+                return Ok($"Customer {customer.Name} with id = {customer.Id} has been deleted.");
+            }
+            else
+            {
+                return Ok($"Customer {customer.Name} with id = {id} never exist");
+            }
+        }
+        
+        [ResponseCache(Duration = 3600)]
+        [HttpGet("{customerId}/addressbooks")]
+        public AddressBooks GetOptions(Guid customerId)
+        {
+            var found = _cache.TryGetValue("AddressBooks", out AddressBooks addressBooks);
+            if (found)
+            {
+                var result = addressBooks.Items.Where(item => item.CustomerId == customerId);
+                if (result.Any())
+                {
+                    return new AddressBooks(result.ToList());
+                }
+            }
+            else
+            {
+                _cache.Set("AddressBooks", new AddressBooks(), new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(5)));
+            }
+
+            return new AddressBooks(customerId);
+        }
     }
 }
